a075b6a60ab9839949abf1c0cf328c99fbf6aaaa
svsaurabh <svsaurabh3232@gmail.com>
2021-06-09 20:16:12 +0530

    Test req

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const axios = require('axios');
 const dateformat = require('dateformat');
 const schedule = require('node-schedule');
+const got = require('got');
 
 const getStates = async (district, currentDate) => {
   try {
     axios({
       method: 'get',
-      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${currentDate}`,
+      url: `https://cdn-api.co-vin.in/api/v2/admin/location/states`,
       headers: { accept: 'application/json' },
     }).then((response) => {
-      console.log(JSON.stringify(response.data.sessions.length));
-      let data = response.data.sessions;
-      data = data.filter((center) => {
-        return center.available_capacity_dose1 > 0 ? true : false;
-      });
-      console.log(data.length);
+      console.log(response);
+      // let data = response.data.sessions;
+      // data = data.filter((center) => {
+      //   return center.available_capacity_dose1 > 0 ? true : false;
+      // });
+      // console.log(data.length);
     });
   } catch (err) {
     console.log('Error :', err);
   }
 };
 // runs every minutes with cron expression
-const job = schedule.scheduleJob('0 * * * * *', () => {
-  getStates('664', dateformat(new Date(), 'dd-mm-yyyy'));
-});
+// const job = schedule.scheduleJob('* * * * * *', () => {
+//   getStates('664', dateformat(new Date(), 'dd-mm-yyyy'));
+// });
+
+(async () => {
+  console.log(await got.get('https://cdn-api.co-vin.in/api/v2/admin/location/states'));
+})();
