de5db9ab5d6e4cdfb065c0cb389050861e6be572
sv-shivansh <sv_shivansh@outlook.com>
2021-06-09 19:44:34 +0530

    try

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const dateformat = require('dateformat');
 const schedule = require('node-schedule');
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
-require('dotenv').config()
-var count= 1
-let prevStateKanpur = [0,0]
-let prevStateMbd = [0,0]
-let prevStateAgra = [0,0]
+require('dotenv').config();
+var count = 1;
+let prevStateKanpur = [0, 0];
+let prevStateMbd = [0, 0];
+let prevStateAgra = [0, 0];
 
 const getStates = async (district, currentDate, prevState) => {
   try {
@@ -27,22 +27,35 @@ const getStates = async (district, currentDate, prevState) => {
       data.map((center) => {
         center.min_age_limit === 45 ? data45.push(center) : data18.push(center);
       });
-      data18.length > 0 ? prevState[0] !== data18.length ?prevState[0]= changeState(prevState[0], data18) : console.log('same data') :console.log("No 18+ record Found")
-      data45.length > 0 ? prevState[1] !== data45.length ?prevState[1] = changeState(prevState[1],data45) : console.log('same data') :console.log("No 45+ record Found")
+      data18.length > 0
+        ? prevState[0] !== data18.length
+          ? (prevState[0] = changeState(prevState[0], data18))
+          : console.log('same data')
+        : console.log('No 18+ record Found');
+      data45.length > 0
+        ? prevState[1] !== data45.length
+          ? (prevState[1] = changeState(prevState[1], data45))
+          : console.log('same data')
+        : console.log('No 45+ record Found');
     });
   } catch (err) {
     console.log('Error :', err);
   }
-  return prevState
+  return prevState;
 };
 
 const changeState = (state, data) => {
-  var val = state
-  console.log("mailing to "+data[0].district_name+ data[0].min_age_limit + `changed from ${val} to ${data.length}`)
+  var val = state;
+  console.log(
+    'mailing to ' +
+      data[0].district_name +
+      data[0].min_age_limit +
+      `changed from ${val} to ${data.length}`
+  );
   val = data.length;
-  mailing(data)
-  return val
-}
+  mailing(data);
+  return val;
+};
 
 async function mailing(data) {
   let transporter = nodemailer.createTransport(
@@ -83,17 +96,20 @@ async function mailing(data) {
     });
     return text;
   };
-
-  let info = await transporter.sendMail({
-    from: {
-      name: 'Notifier',
-      address: 'hritzz12345@gmail.com',
-    },
-    to: ['i10shivansh@gmail.com'],
-    subject: `${data[0].district_name} ${data[0].state_name} Available Slots ${data[0].min_age_limit}+`,
-    text: generatetext(),
-  });
-  console.log('Message sent: %s', info.messageId);
+  try {
+    let info = await transporter.sendMail({
+      from: {
+        name: 'Notifier',
+        address: 'hritzz12345@gmail.com',
+      },
+      to: ['i10shivansh@gmail.com'],
+      subject: `${data[0].district_name} ${data[0].state_name} Available Slots ${data[0].min_age_limit}+`,
+      text: generatetext(),
+    });
+    console.log('Message sent: %s', info.messageId);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 const fetchResponse = async () => {
@@ -106,5 +122,4 @@ const fetchResponse = async () => {
 //   console.log(`${count++} call`)
 // });
 
-fetchResponse()
-
+fetchResponse();
