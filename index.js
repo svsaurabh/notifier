c5aeaebe6271647119bb029880ce02ebb4f6e19a
sv-shivansh <sv_shivansh@outlook.com>
2021-06-09 20:29:49 +0530

    https agent

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const axios = require('axios');
 const dateformat = require('dateformat');
 const schedule = require('node-schedule');
+const https = require('https')
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 const getStates = async (district, currentDate) => {
   try {
-    axios({
-      method: 'get',
-      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${currentDate}`,
-      headers: { accept: 'application/json' },
-    }).then((response) => {
+    let response = await axios.get(
+      `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${currentDate}`,
+      {
+        httpsAgent: agent,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': '*',
+        },
+      }
+    );
       console.log(JSON.stringify(response.data.sessions.length));
       let data = response.data.sessions;
       data = data.filter((center) => {
         return center.available_capacity_dose1 > 0 ? true : false;
       });
       console.log(data.length);
-    });
   } catch (err) {
     console.log('Error :', err);
   }
 };
 // runs every minutes with cron expression
-const job = schedule.scheduleJob('0 * * * * *', () => {
-  getStates('664', dateformat(new Date(), 'dd-mm-yyyy'));
-});
+// const job = schedule.scheduleJob('0 * * * * *', () => {
+//   getStates('664', dateformat(new Date(), 'dd-mm-yyyy'));
+// });
+getStates('664', dateformat(new Date(), 'dd-mm-yyyy'));
