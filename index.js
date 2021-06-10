8906cb72a9c412ae262b8977430b036bec34e460
svsaurabh <svsaurabh3232@gmail.com>
2021-06-10 22:24:17 +0530

    Included header

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const axios = require('axios');
 const dateformat = require('dateformat');
 const schedule = require('node-schedule');
 const got = require('got');
+const ProxyList = require('free-proxy');
+const proxyList = new ProxyList();
+let proxies;
 
 const getStates = async (district, currentDate) => {
   try {
     axios({
       method: 'get',
       url: `https://cdn-api.co-vin.in/api/v2/admin/location/states`,
-      headers: { accept: 'application/json' },
+      headers: {
+        accept: 'application/json',
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+      },
     }).then((response) => {
-      console.log(response);
+      console.log(response.data.states);
       // let data = response.data.sessions;
       // data = data.filter((center) => {
       //   return center.available_capacity_dose1 > 0 ? true : false;
@@ -27,5 +34,8 @@ const getStates = async (district, currentDate) => {
 // });
 
 (async () => {
-  console.log(await got.get('https://cdn-api.co-vin.in/api/v2/admin/location/states'));
+  getStates('664', dateformat(new Date(), 'dd-mm-yyyy'));
+  // await got.get('https://cdn-api.co-vin.in/api/v2/admin/location/states').then((resp) => {
+  //   console.log(resp.body);
+  // });
 })();
